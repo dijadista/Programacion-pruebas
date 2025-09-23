9354fe7e207a9efd5ea88738b3c2070d0ba91be1
dijadista <rubenbm@msmk.university>
2025-09-23 17:06:30 +0200

    cambio prueba 1

--- hola.cpp
+++ hola.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hola, Repositorio!" << std::endl;
+int Add (int x, int y) {
+  std::cout << "En Add(), recibido " <<x<< " y " <<y<< "\n";
+  return (x+y);
+
+}
+
+int main () {
+    std::cout << "En main().\n"; 
+    std::cout << "\nLlamada a Add()\n";
+    std::cout << "El valor devuelto es: " << Add (3,4);
+    std::cout << "\nvuelta a main().\n";
+    std::cout << "\nsaliendo..\n";
     return 0;
 }
